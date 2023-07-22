709084ca605fe2732a6a698a6483fefacbfea01e
YousefMaher179 <ym1233445@gmail.com>
2023-07-22 15:00:56 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useContext } from "react";
+
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
+import AuthContext from "./Store/auth-context";
+// import AuthContext from "./Store/auth-context";
 
 function App() {
+  const cta = useContext(AuthContext);
+  // const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  // useEffect(() => {
+  //   const storedUserLoggedOnInformation = localStorage.getItem("isLoggedIn");
+
+  //   if (storedUserLoggedOnInformation === "1") {
+  //     setIsLoggedIn(true);
+  //   }
+  // }, []);
+  // const loginHandler = (email, password) => {
+  //   // We should of course check email and password
+  //   // But it's just a dummy/ demo anyways
+  //   localStorage.setItem("isLoggedIn", "1");
+  //   setIsLoggedIn(true);
+  // };
+
+  // const logoutHandler = () => {
+  //   localStorage.removeItem("isLoggedIn");
+  //   setIsLoggedIn(false);
+  // };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <AuthContext.Provider
+    //   value={{
+    //     isLoggedIn: isLoggedIn,
+    //     onLogout: logoutHandler,
+    //   }}
+    // >
+    <React.Fragment>
+      <MainHeader />
+      <main>
+        {!cta.isLoggedIn && <Login />}
+        {cta.isLoggedIn && <Home />}
+      </main>
+    </React.Fragment>
+    // </AuthContext.Provider>
   );
 }
 
---
+++ src/components/UI/Input/Input.js
@@ -0,0 +1,36 @@
+import React, { useRef, useImperativeHandle } from "react";
+
+import classes from "./Input.module.css";
+
+const Input = React.forwardRef((props, ref) => {
+  const inpuRef = useRef();
+
+  const activate = () => {
+    inpuRef.current.focus();
+  };
+
+  useImperativeHandle(ref, () => {
+    return {
+      focus: activate,
+    };
+  });
+  return (
+    <div
+      className={`${classes.control} ${
+        props.isValid === false ? classes.invalid : ""
+      }`}
+    >
+      <label htmlFor={props.id}>{props.label}</label>
+      <input
+        ref={inpuRef}
+        type={props.type}
+        id={props.id}
+        value={props.value}
+        onChange={props.onChange}
+        onBlur={props.onBlur}
+      />
+    </div>
+  );
+});
+
+export default Input;
